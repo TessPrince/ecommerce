73319956ef2cd0ed1ed1ed60aaf4ba118e1ab089
TessPrince <94005300+TessPrince@users.noreply.github.com>
2022-11-07 19:35:25 -0600

    Created Customers Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
   get 'soaps/index'
   get 'soaps/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
