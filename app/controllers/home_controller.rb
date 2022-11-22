955161264be7556f2c24d02c24b61d9d593214b2
TessPrince <94005300+TessPrince@users.noreply.github.com>
2022-11-22 15:41:31 -0600

    Removed Un-needed code for search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,4 @@ class HomeController < ApplicationController
     @soaps = Soap.page(params[:page])
     @categories = Category.all
   end
-
-  def search
-    wildcard = "%#{params[:keywords]}%"
-    category_wildcard = params[:categories]
-
-    if category_wildcard != ""
-      @soaps = Soap.joins(:categories).where("name LIKE ? AND category_id = ?", wildcard, category_wildcard).page(params[:page])
-   else
-      @soaps = Soap.where("name LIKE ?", wildcard)
-   end
-  end
 end
