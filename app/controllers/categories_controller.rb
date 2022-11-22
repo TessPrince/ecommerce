f1983e595e35d2102e7de46920113ac6f3b95e4f
TessPrince <94005300+TessPrince@users.noreply.github.com>
2022-11-22 16:42:26 -0600

    Added Categories Page (Index and Show)
    Can navigate to categories page through nav to see list of categories, can click on a category to go to the show page that shows all the products associated with said category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.page(params[:page])
   end
 
   def show
+    @category = Category.find(params[:id])
+    @soaps = @category.soaps
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   end
 end
 
+resources :categories, only: [:index, :show]
 
   # Defines the root path route ("/")
   root to: "home#index"
