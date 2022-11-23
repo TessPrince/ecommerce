25fbb80ec5ed8c23fc602366311fe86ff7187ad5
TessPrince <94005300+TessPrince@users.noreply.github.com>
2022-11-23 12:10:43 -0600

    Implemented cart delete

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -6,14 +6,18 @@ class CartController < ApplicationController
     unless session[:shopping_cart].include?(id)
       session[:shopping_cart] << id #pushs the id onto the end of the array
       soap = Soap.find(id)
-      flash[:notice] = " #{soap.name} added to cart."
+      flash[:notice] = " + #{soap.name} added to cart."
     end
 
     redirect_to root_path
   end
 
   def destroy
-
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    soap = Soap.find(id)
+    redirect_to root_path
+    flash[:notice] = " - #{soap.name} removed from cart."
   end
 
 end
