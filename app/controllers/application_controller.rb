fba5dcb16936c38bf63aaad4578d6df3f4ecea55
TessPrince <94005300+TessPrince@users.noreply.github.com>
2022-12-06 11:37:57 -0600

    Added access to edit page and added address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :initialize_session
   helper_method :cart
 
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:address, :province_id, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit( :address, :province_id, :email, :password, :password_confirmation, :current_password) }
+  end
+
   private
   def initialize_session
     session[:shopping_cart] ||= [] #empty array of product IDs
